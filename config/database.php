3ea8719b5479185146d42ef93968bc2fc237fd51
tapos ghosh <tapos.aa@gmail.com>
2017-06-09 22:57:16 +0600

    sdfsf

--- config/database.php
+++ config/database.php
@@ -56,13 +56,15 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => parse_url(getenv("DATABASE_URL"))["host"],
+            'host'     => parse_url(getenv("DATABASE_URL"))["host"],
+            'port' => env('DB_PORT', '5432'),
             'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
             'username' => parse_url(getenv("DATABASE_URL"))["user"],
             'password' => parse_url(getenv("DATABASE_URL"))["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
+            'sslmode' => 'prefer',
         ],
 
         'sqlsrv' => [
